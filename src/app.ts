9442dcaac863745223aa9597ac24a77d9c07c457
codingsprints <codingsprint31@gmail.com>
2025-09-30 20:03:08 +0530

    added middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express, { Request, Response } from 'express';
 import { globalErrorHandler } from './middleware/globalMiddleware';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
